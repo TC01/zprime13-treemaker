fc227f7eee98ed3298062737fedb6e3da1f807f1
Ben Rosser <bjr@fnal.gov>
2015-09-04 00:04:17 -0500

    run.py loops over the source directory and runs the right things.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 from Zprime_Inclusive_Treemaker import Zprime_Inclusive_Treemaker
 
 def run(source, output, isData):
@@ -8,5 +10,28 @@ def run(source, output, isData):
 	treemaker.Fill('B2GTTreeMaker/B2GTree')
 	print "*** Cleaning up..."
 
+def main(directory):
+	# I *was* going to specify everything we needed but I got lazy...
+	for path, dirs, files in os.walk(directory):
+		if path == directory:
+			for dir in dirs:
+				outputName = dir
+				source = os.path.join(directory, dir)
+
+				# Decide which things are data.
+				isData = False
+				if "SingleElectron" or "SingleMu" in outputName:
+					isData = True
+				
+				# Decide which things to exclude-- we only want the PromptReco data, I think?
+				if isData and '17Jul2015' in outputName:
+					continue
+
+				run(source + "/", outputName, isData)
+
+
 if __name__ == '__main__':
-	run("/uscms_data/d3/jkarancs/B2GTTreeNtuple/Aug13/SingleElectron_Run2015B-PromptReco/", "test", False)
+	# Test code.
+	#run("/uscms_data/d3/jkarancs/B2GTTreeNtuple/Aug13/SingleElectron_Run2015B-PromptReco/", "test", False)
+	
+	main("/uscms_data/d3/jkarancs/B2GTTreeNtuple/Aug13/")
